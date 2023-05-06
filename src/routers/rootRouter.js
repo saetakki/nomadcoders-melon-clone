e8b994c109bcac0be7a3f3bbf921f4f58e2037d7
aaron93 <lime.lemonade03@gmail.com>
2023-05-07 04:17:03 +0900

    refactor: Make the router dynamically
    routerData에 담긴 데이터를 map 매서드로 순회하며 동적으로 라우터 생성

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,29 @@
 import express from "express";
-import { home } from "../controllers/songController";
+import { renderPage } from "../controllers/renderController";
 
 const rootRouter = express.Router();
+const routerData = [
+  {
+    path: "/",
+    label: "Home",
+    element: "home",
+  },
+  {
+    path: "/test",
+    label: "Test",
+    element: "test",
+  },
+  {
+    path: "/user",
+    label: "User Info",
+    element: "userInfo",
+  },
+];
 
-rootRouter.get("/", home);
+routerData.map((router) => {
+  rootRouter.get(router.path, (res, req) => {
+    renderPage(res, req, router.label, router.element);
+  });
+});
 
 export default rootRouter;
